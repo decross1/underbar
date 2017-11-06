137f20990c13ad2656a26baa187f73d317543e7d
Derrick Cross <decross1@me.com>
2017-11-06 12:11:06 -0800

    finished memoize function

--- src/underbar.js
+++ src/underbar.js
@@ -356,6 +356,18 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var results = {};
+    
+    return function() {
+      var args = Array.prototype.slice.call(arguments);
+      var key = JSON.stringify(args);
+
+      if (!(key in results)) {
+        results[key] = func.apply(this, args);
+      }
+
+      return results[key];
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
